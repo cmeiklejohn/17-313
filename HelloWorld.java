eb8b6d4c938c9fb1bd77bef8b43e598a767ef3d9
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2019-08-29 12:02:15 -0400

    Initial!

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] argv) {
+        System.out.println("Hello World, Christopher!  I have been a Ph.D. student for way too long.")
+    }
+}
