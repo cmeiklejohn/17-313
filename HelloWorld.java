013905f97171e793043bb36556f869788f0e6d63
karenak-cmu <karenak@andrew.cmu.edu>
2019-08-29 12:08:13 -0400

    I made a change too.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] argv) {
         System.out.println("Hello World, Christopher!  I have been a Ph.D. student for way too long.")
+        System.out.println("Karena was here");
     }
 }
